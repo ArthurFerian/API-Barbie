7133bd2f505522cc89a2e64ffbb4f0af1bb5f007
Arthur Ferian <arthur.ferian@aluno.senai.br>
2025-09-12 16:17:38 -0300

    first commit

---
+++ src/controllers/barbieControllers.js
@@ -0,0 +1,71 @@
+import dados from "./../models/dados.js";
+const { barbies } = dados;
+
+const getAllBarbies = (req, res) => {
+  res.status(200).json({
+    total: barbies.length,
+    barbies: barbies,
+  });
+};
+
+const getBarbieById = (req, res) => {
+  let id = parseInt(req.params.id);
+
+  const barbie = barbies.find((b) => b.id === id);
+
+  res.status(200).json({
+    sucess: true,
+    barbie: barbie,
+  });
+};
+
+const createBarbie = (req, res) => {
+  const { nome, profissao, anoLancamento } = req.body;
+
+  if (!nome || !profissao) {
+    return res.status(400).json({
+      sucess: false,
+      message: "Nome e Profissão são obrigatorios!!!",
+    });
+  }
+
+  const novaBarbie = {
+    id: barbies.length + 1,
+    nome: nome,
+    profissao: profissao,
+    anoLancamento: anoLancamento,
+  };
+
+  barbies.push(novaBarbie);
+
+  res.status(201).json({
+    sucess: true,
+    message: "Barbie cadastrada com sucesso!",
+    barbie: novaBarbie,
+  });
+};
+
+const deleteBarbie = (req, res) => {
+  let id = parseInt(req.params.id);
+
+  const barbieParaRemover = barbies.find((b) => b.id === id);
+
+  if (!barbieParaRemover) {
+    return res.status(404).json({
+      sucess: false,
+      message: `Essa barbie não existe, ${id}`,
+    });
+  }
+
+  const barbiesFiltradas = barbies.filter((barbie) => barbie.id !== id);
+
+  barbies.splice(0, barbies.length, ...barbiesFiltradas);
+
+  res.status(200).json({
+    sucess: true,
+    message: "A barbie foi removida com sucesso",
+    barbieRemovida: barbieParaRemover,
+  });
+};
+
+export { getAllBarbies, getBarbieById, createBarbie, deleteBarbie };
---
+++ src/routes/barbieRoutes.js
@@ -0,0 +1,16 @@
+import express from "express";
+import {
+  createBarbie,
+  deleteBarbie,
+  getAllBarbies,
+  getBarbieById,
+} from "./../controllers/barbieControllers.js";
+
+const router = express.Router();
+
+router.get("/", getAllBarbies);
+router.get("/:id", getBarbieById);
+router.post("/", createBarbie);
+router.delete("/:id", deleteBarbie);
+
+export default router;
